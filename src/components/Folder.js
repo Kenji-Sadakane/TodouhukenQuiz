8f7c4d4a1ac900708c8b01b474263621ab0e6665
Kenji Sadakane <kenji.sadakane@hmcom.co.jp>
2019-01-28 01:13:33 +0900

    fontawesomeの読み込み追加、ドラッグイベント追加

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -6,11 +6,51 @@ class Folder extends Component {
   constructor(props) {
     super(props);
     this.state = { open: false }
-    this.handleChange = this.handleChange.bind(this);
+    this.bindEvent();
   }
 
-  handleChange() {
+  bindEvent() {
+    this.onCheckChange = this.onCheckChange.bind(this);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragOver = this.onDragOver.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  componentDidMount() {
+  }
+
+  onCheckChange(e) {
     this.setState({ open: !this.state.open });
+  }
+
+  onDragStart(e) {
+    console.log('start');
+  }
+
+  onDragEnter(e) {
+    console.log('enter');
+  }
+
+  onDragOver(e) {
+    // console.log('over');
+  }
+
+  onDragLeave(e) {
+    console.log('leave');
+  }
+
+  onDragEnd(e) {
+    console.log('End');
+  }
+
+  onDrop(e) {
+    console.log('drop');
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('drop');
   }
 
   render() {
@@ -18,9 +58,10 @@ class Folder extends Component {
     const { open } = this.state;
 
     return (
-      <Wrapper ggg>
-        <div className="column folder" draggable="true">
-          <input type="checkbox" id={id} checked={open} onChange={this.handleChange} className="folderCheck" />
+      <Wrapper>
+        <div className="column folder" draggable="true" onDragStart={this.onDragStart} onDragEnter={this.onDragEnter}
+         onDragOver={this.onDragOver} onDragLeave={this.onDragLeave} onDragEnd={this.onDragEnd} onDrop={this.onDrop} >
+          <input type="checkbox" id={id} checked={open} onChange={this.onCheckChange} className="folderCheck" />
           <label htmlFor={id}>{name}</label>
         </div>
         <div className="accordion" open={open}>
@@ -38,8 +79,18 @@ const Wrapper = styled.div`
   input[type="checkbox"] {
     display: none;
   }
+  input[type="checkbox"] + label:before {
+    font-family: 'Font Awesome 5 Free';
+    font-weight: bold;
+    content: '\f105';
+    padding-right: 8px;
+  }
+  input[type="checkbox"]:checked + label:before {
+    content: '\f107';
+  }
   .accordion {
-    padding-left: ${props => props.ggg ? 3 : 0}em;
+    // padding-left: ${props => props.ggg ? 3 : 0}em;
+    padding-left: 2em;
     opacity: 0;
     max-height: 0px;
     overflow-y: hidden;
