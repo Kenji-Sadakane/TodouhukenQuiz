46fab44e285612d2ad1e7cc856fe3fbc5b38b553
vagrant <vagrant@vagrant.vm>
2019-01-28 09:36:16 +0000

    bookmarkにドラッグイベント追加

--- src/components/Bookmark.js
+++ src/components/Bookmark.js
@@ -1,9 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Bookmark = ({id, name, url}) => (
-  <div id={id} className="column" draggable="true">
-    <li><a href={url} target="_blank">{name}</a></li>
-  </div>
-);
+class Bookmark extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false }
+    this.bindEvent();
+  }
+
+  bindEvent() {
+    this.onCheckChange = this.onCheckChange.bind(this);
+    this.onDragStart = this.onDragStart.bind(this);
+    this.onDragEnter = this.onDragEnter.bind(this);
+    this.onDragOver = this.onDragOver.bind(this);
+    this.onDragLeave = this.onDragLeave.bind(this);
+    this.onDragEnd = this.onDragEnd.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  componentDidMount() {
+  }
+
+  onCheckChange(e) {
+    this.setState({ open: !this.state.open });
+  }
+
+  onDragStart(e) {
+    console.log('start');
+  }
+
+  onDragEnter(e) {
+    console.log('enter');
+  }
+
+  onDragOver(e) {
+    // console.log('over');
+  }
+
+  onDragLeave(e) {
+    console.log('leave');
+  }
+
+  onDragEnd(e) {
+    console.log('End');
+  }
+
+  onDrop(e) {
+    console.log('drop');
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('drop');
+  }
+
+  render() {
+    const { id, name, url } = this.props;
+
+    return (
+      <div id={id} className="column" draggable="true" onDragStart={this.onDragStart} onDragEnter={this.onDragEnter}
+         onDragOver={this.onDragOver} onDragLeave={this.onDragLeave} onDragEnd={this.onDragEnd} onDrop={this.onDrop} >
+        <li><a href={url} target="_blank">{name}</a></li>
+      </div>
+    );
+  }
+}
 
 export default Bookmark;
